93b0b697e85034c5a3466078bcd1aeb93bd72a7d
Taulant Culi <t.culi91@gmail.com>
2021-04-26 17:01:26 +0200

    ceating dummy event data

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,11 +1,26 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const HomePage = () => {
+export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Home Page Component</h1>
+      {events.length === 0 && <h3>There are no events!</h3>}
+      {events.map((evt) => (
+        <img src={evt.image} style={{ maxWidth: "33%" }}></img>
+      ))}
     </Layout>
   );
-};
+}
 
-export default HomePage;
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+
+  const events = await res.json();
+
+  return {
+    props: { events }, // will be passed to the page component as props
+    revalidate: 1, // In seconds
+  };
+}
