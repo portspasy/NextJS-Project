26339f199421f0d618d515655eaefdc843c724ce
Taulant Culi <t.culi91@gmail.com>
2021-04-26 14:10:09 +0200

    creating Module Aliases with jsconfig

--- my-app/pages/404.js
+++ my-app/pages/404.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
-import Layout from "../components/Layout";
-import styles from "../styles/404.module.css";
+import Layout from "@/components/Layout";
+import styles from "@/styles/404.module.css";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
   return (
     <Layout title="Page Not Found">
       <div className={styles.error}>
-        <h1>404 </h1>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
         <h2>Page Not Found!!!</h2>
         <p>
           <Link href="/">
